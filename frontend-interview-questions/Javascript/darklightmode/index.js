fa7226dcd16862fe6120d97dad879437d3d0584a
nam334 <namratadas334@gmail.com>
2026-06-28 17:30:14 +0530

    solved 2nd part of Dark Light Mode Question

--- frontend-interview-questions/Javascript/darklightmode/index.js
+++ frontend-interview-questions/Javascript/darklightmode/index.js
@@ -1,5 +1,8 @@
 const container = document.querySelector(".container");
 const button = document.querySelector(".button");
+const toggleswitch = document.querySelector(".switch");
+const toggle = document.querySelector(".toggle");
+
 button.addEventListener("click", () => {
   console.log(button.innerText);
   if (button.innerText === "Light") {
@@ -10,5 +13,15 @@ button.addEventListener("click", () => {
     container.classList.replace("dark", "light");
     button.classList.replace("lightbtn", "darkbtn");
     button.innerText = "Light";
+  }
+});
+
+toggleswitch.addEventListener("click", () => {
+  if (toggleswitch.classList.contains("left")) {
+    container.classList.replace("light", "dark");
+    toggleswitch.classList.replace("left", "right");
+  } else if (toggleswitch.classList.contains("right")) {
+    container.classList.replace("dark", "light");
+    toggleswitch.classList.replace("right", "left");
   }
 });
