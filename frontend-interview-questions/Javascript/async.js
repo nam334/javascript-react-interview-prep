09c95445c8f576ea9b4be11165ac876d9af2dec7
nam334 <namratadas334@gmail.com>
2026-09-03 20:19:50 +0530

    solved polyfills nd other concepts

---
+++ frontend-interview-questions/Javascript/async.js
@@ -0,0 +1,5 @@
+console.log("ASYNC script executed");
+
+// const element = document.getElementById("message");
+
+// console.log("Can async find the element?", element);
---
+++ frontend-interview-questions/Javascript/defer.js
@@ -0,0 +1,5 @@
+console.log("DEFER script executed");
+
+const element = document.getElementById("message");
+
+console.log("Can defer find the element?", element);
