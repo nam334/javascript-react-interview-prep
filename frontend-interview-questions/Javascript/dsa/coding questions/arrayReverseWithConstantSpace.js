049bf445c917016901f078a6406a9b66d05286fd
nam334 <namratadas334@gmail.com>
2026-09-25 14:10:46 +0530

    Solved reversing  an array with Space Complexity O(1)

---
+++ frontend-interview-questions/Javascript/dsa/coding questions/arrayReverseWithConstantSpace.js
@@ -0,0 +1,19 @@
+// function reverseArray(arr) {
+//   let start = 0,
+//     end = arr.length - 1;
+//   while (start < end) {
+//     let temp = arr[start];
+//     arr[start] = arr[end];
+//     arr[end] = temp;
+//     start++;
+//     end--;
+//   }
+//   return arr;
+// }
+// console.log(reverseArray([])); // []
+// console.log(reverseArray([7])); // [7]
+// console.log(reverseArray([1, 1, 2])); // [2, 1, 1]
+// console.log(reverseArray([-3, 0, 5])); // [5, 0, -3]
+
+// TC - O(N);
+// SC - O(1);
